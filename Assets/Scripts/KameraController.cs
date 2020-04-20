827527d362da40b98929b2412feabc426020ef72
theMateoK <44950237+theMateoK@users.noreply.github.com>
2020-04-21 00:15:44 +0200

    Initial commit

---
+++ Assets/Scripts/KameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 pomak;
+    void Start()
+    {
+        pomak = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + pomak;
+    }
+}
---
+++ Assets/Scripts/Kretanje.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Kretanje : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public Text countText;
+    private int count;
+    public Text winText;
+    public Text againText;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        againText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Rezultat: " + count.ToString();
+        if (count >= 16)
+        {
+            winText.text = "Pobjedio si!";
+            againText.text = "Pokušaj ponovo";
+        }
+    }
+}
